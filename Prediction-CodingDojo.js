5a4350a0af2ecb7c89d9c084cbf8efaf788caa64
DonDiabeton <102296807+DonDiabeton@users.noreply.github.com>
2022-04-28 10:32:13 -0400

    Add files via upload

---
+++ Prediction-CodingDojo.js
@@ -0,0 +1,13 @@
+function add(num1, num2){
+        console.log("¡Sumando números!");
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+
+    Numeros     Variables
+    -num1           num1 + num1
+    -num2           num2 + num2
+    
+    
